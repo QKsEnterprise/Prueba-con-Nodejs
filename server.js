7381dfcf913a5a5b426e6eafa9decac687721e33
Kuka <martinelliluca@gmail.com>
2021-01-11 20:31:31 -0300

    Cree el primer servidor

---
+++ server.js
@@ -0,0 +1,14 @@
+const { text } = require("express");
+const http = require("http");
+
+const server = function(rep, res) {
+    res.writeHead(200, {'Content-type': 'text/html' });
+    res.write( '<h1>This Is My First Server/h1>');
+    res.end();
+}
+
+const servidor = http.createServer(server);
+
+servidor.listen(3000, function () {
+    console.log('server is on port 3000');
+})
